0477ce809e124f449db1073fe78ba24585496801
StarryPurple <starrypurple@163.com>
2025-02-06 21:54:32 +0800

    upd: Unexpectancy occurs. Fixing it.

--- main.cpp
+++ main.cpp
@@ -92,11 +92,10 @@ int main() {
   // freopen("../answer.txt", "w", stdout);
   try {
     // test_filestream();
-    // test_database();
+    test_database();
   } catch(Insomnia::FileSystemException &ex) {
     std::cout << ex.what() << std::endl;
   }
-  test_database();
-  // fclose(stdin); fclose(stdout);
+  // test_database();
   return 0;
 }
